53e8df4475fe059013e6e34b0f8d4e14cd1224d5
Kurt Bauer <kurtbauer55@protonmail.com>
2019-01-04 12:33:15 -0500

    wrote firs test

--- src/App.test.js
+++ src/App.test.js
@@ -5,5 +5,12 @@ import App from './App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  // adding code under render
+
+  // now that test is written, try in terminal
+  expect(div.innerHTML).toContain('HI THERE!');
+
   ReactDOM.unmountComponentAtNode(div);
 });
+
+// TODO: this file came with boilerplate code
